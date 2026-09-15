b798830ffd5123b36140df7d59dd455d1207c378
olsontjo <43152332+olsontjo@users.noreply.github.com>
2026-09-15 11:11:43 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "=3.42.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "=3.42.0"
+    }
+  }
+}
 
 provider "aws" {
   region  = var.region
