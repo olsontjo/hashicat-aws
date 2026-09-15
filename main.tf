7148a77ba2d2a18cef07df778fb43dc79c27749f
olsontjo <43152332+olsontjo@users.noreply.github.com>
2026-09-15 15:40:36 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "=3.42.0"
-     }
-    }
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "=3.42.0"
+#     }
+#    }
   backend "remote" {} 
 }
 
